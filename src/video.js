57e3de5712899a42c37632609c7bfd9cd40e3a90
Rameesha55 <rameeshasiddiqui03@gmail.com>
2024-11-29 16:34:13 +0500

    first commit

---
+++ src/video.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Video(){
+    return(
+        <div className="video">
+            <video src="https://www.tiktok.com/@moonlight_editz9/video/7275638118177443078?q=taylor%20swift&t=1732877109418"></video>
+
+        </div>
+    )
+}
+
+export default Video
